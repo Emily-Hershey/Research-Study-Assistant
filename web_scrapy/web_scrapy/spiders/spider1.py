c6eb72a9752ab91b9e6a65cfe89bae6303efec6f
Emily Hershey <ehershe@purdue.edu>
2024-06-03 00:11:26 -0400

    Adds webscrapped data to SQL database

--- web_scrapy/web_scrapy/spiders/spider1.py
+++ web_scrapy/web_scrapy/spiders/spider1.py
@@ -1,7 +1,7 @@
 import scrapy
 from bs4 import BeautifulSoup 
 import requests 
-
+import sqlite3
 
 class Spider1(scrapy.Spider):
     name = "spider1"
@@ -15,7 +15,29 @@ class Spider1(scrapy.Spider):
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
+    
     def parse(self, response):
-        paragraphs = response.xpath('//p/text()').getall()
-        print("Paragraphs:", paragraphs)
+        paragraphs_list = response.xpath('//p/text()').getall()
+        #print("Paragraphs:", paragraphs)
+        paragraphs_text = ' '.join(paragraphs_list)
+        
+        conn = sqlite3.connect('example.db')  # 'example.db' is the database file
+        cursor = conn.cursor()  # cursor object allows you to execute SQL commands
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS websites (
+                topic TEXT,
+                text TEXT
+            )
+        ''')
+        cursor.execute('''
+            INSERT INTO websites (topic, text) VALUES ('history', ?)
+        ''', (paragraphs_text,))
+        conn.commit()
+        cursor.execute('SELECT * FROM websites')
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        conn.close()
+
+
 
